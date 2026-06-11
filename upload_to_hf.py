dd9270e73ee5f7b7702215b3fe2c8cdb5753bc0a
Islam SadAldeen <islamsadaldeen02@gmail.com>
2026-06-12 01:14:30 +0300

    Update upload_to_hf.py

--- upload_to_hf.py
+++ upload_to_hf.py
@@ -3,14 +3,14 @@ from huggingface_hub import HfApi
 api = HfApi()
 
 api.create_repo(
-    repo_id="1aRR0w1/scientific-abstract-evaluator-lora",
+    repo_id = "IslamSaadAldeen/scientific-abstract-evaluator-lora",
     repo_type="model",
     exist_ok=True,
 )
 
 api.upload_folder(
     folder_path="hf_upload/scientific-abstract-evaluator-lora",
-    repo_id="1aRR0w1/scientific-abstract-evaluator-lora",
+    repo_id = "IslamSaadAldeen/scientific-abstract-evaluator-lora",
     repo_type="model",
 )
 
